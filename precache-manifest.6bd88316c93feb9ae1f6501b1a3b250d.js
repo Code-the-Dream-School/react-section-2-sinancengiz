8a3dfd81c0cff6819b7be3e89b684134661fb6cb
Sinan Cengiz <sinancengiz_84@hotmail.com>
2020-07-31 14:56:21 -0700

    Updates

---
+++ precache-manifest.6bd88316c93feb9ae1f6501b1a3b250d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe3c3384400f34c79770b2632b60fd7",
+    "url": "/react-section-2-sinancengiz/index.html"
+  },
+  {
+    "revision": "453d3a82c96aa3e7b4ba",
+    "url": "/react-section-2-sinancengiz/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c17f7e79ae4e21b525ff",
+    "url": "/react-section-2-sinancengiz/static/css/main.4f71a5f6.chunk.css"
+  },
+  {
+    "revision": "453d3a82c96aa3e7b4ba",
+    "url": "/react-section-2-sinancengiz/static/js/2.2e079255.chunk.js"
+  },
+  {
+    "revision": "8ced3e7d2377db1986992934dc6262e2",
+    "url": "/react-section-2-sinancengiz/static/js/2.2e079255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c17f7e79ae4e21b525ff",
+    "url": "/react-section-2-sinancengiz/static/js/main.99e6c1df.chunk.js"
+  },
+  {
+    "revision": "05b6d120057886065bb8",
+    "url": "/react-section-2-sinancengiz/static/js/runtime-main.d2958814.js"
+  },
+  {
+    "revision": "b6fb990b36d14052b565fb47ae3dd7be",
+    "url": "/react-section-2-sinancengiz/static/media/Sinan_Cengiz_Resume.b6fb990b.pdf"
+  },
+  {
+    "revision": "50c485cd1da85fca8225e28bd4c70bcc",
+    "url": "/react-section-2-sinancengiz/static/media/laptop_red.50c485cd.jpg"
+  },
+  {
+    "revision": "3e1f0d33a066fa94a08e63ea4f2c1e48",
+    "url": "/react-section-2-sinancengiz/static/media/laptops_blue.3e1f0d33.jpg"
+  },
+  {
+    "revision": "567f4f1fc6713194daace5a168e987c4",
+    "url": "/react-section-2-sinancengiz/static/media/multi_image_1.567f4f1f.png"
+  },
+  {
+    "revision": "bff86574351dfc585c2387a9d190ef45",
+    "url": "/react-section-2-sinancengiz/static/media/sinan_cengiz.bff86574.jpg"
+  }
+]);
